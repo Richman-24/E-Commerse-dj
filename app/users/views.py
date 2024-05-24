18e3a40665870306b7b152ab0b9c137b5fc055e1
Maksim <dreev24@yandex.ru>
2024-05-24 13:02:24 +0300

    login form, controlloer and template are ready

--- app/users/views.py
+++ app/users/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth, messages
+
+from .forms import UserLoginForm
 
 
+def login(request):
+    if request.method == "POST":
+        form = UserLoginForm(data = request.POST)
+        
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+        
+            if user:
+                auth.login(request, user)
+                messages.success(request, f"Приветствуем Вас {username}")
+                return HttpResponseRedirect(reverse("main:index"))
+    else: 
+        form = UserLoginForm()
+    
+    context = {
+        "title":"Авторизация",
+        "form": form,
+    }
 
-def login(request): ...
+    return render(request, "users/login.html", context)
 
 def registration(request): ...
 
